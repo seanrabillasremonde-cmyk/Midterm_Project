beea19bd996c032b8a76377ec228d72418378045
seanrabillasremonde-cmyk <seanrabillas.remonde@dbtc-cebu.edu.ph>
2026-03-10 20:14:23 +0700

    Update Main_menu.cs

--- Main_menu.cs
+++ Main_menu.cs
@@ -2,7 +2,9 @@ using Student_Management;
 using System;
 using System.Diagnostics;
 using System.IO;
-using System.Xml.Linq;
+using System.Text.Json;
+using static System.Net.Mime.MediaTypeNames;
+
 
 class Program
 {
@@ -14,111 +16,111 @@ class Program
        "SchoolSyst" // The name of the folder  EnterGradesvariable
 
 
-   );             
-                                             
-            {                                  
-                string studentSubjectFile = Path.Combine(mainFolder, $"StudentSubject.txt"); // <--- The name of the file EnterGradesvariable
+   );
+
+        {
+            string studentSubjectFile = Path.Combine(mainFolder, $"StudentSubject.txt"); // <--- The name of the file EnterGradesvariable
 
 
-                string gradeFile = Path.Combine(mainFolder, $"Grades.txt"); // <--- The name of the file EnterGradesvariable
+            string gradeFile = Path.Combine(mainFolder, $"Grades.txt"); // <--- The name of the file EnterGradesvariable
 
-                bool running = true;
-                string Menu;
+            bool running = true;
+            string Menu;
+            Console.Clear();
+            while (running)
+            {
+                Console.WriteLine("\n+===== Main   Menu ======+");
+                Console.WriteLine("│1.Register Student      │");
+                Console.WriteLine("│2.Enroll student subject│");
+                Console.WriteLine("│3.Enter Grades          │");
+                Console.WriteLine("│4.Show Grade by student │");
+                Console.WriteLine("│5.Create folder         │");
+                Console.WriteLine("│6.Exit                  │");
+                Console.WriteLine("│════════════════════════│");
+                Console.WriteLine("│Pick a number 1 to 6    │");
+                Console.WriteLine("│Choose:                 │");
+                Console.WriteLine("+========================+");
+
+                Console.SetCursorPosition(8, 10);
+                Menu = Console.ReadLine();
+
                 Console.Clear();
-                while (running)
+
+
+                switch (Menu)
                 {
-                    Console.WriteLine("\n+===== Main   Menu ======+");
-                    Console.WriteLine("│1.Register Student      │");
-                    Console.WriteLine("│2.Enroll student subject│");
-                    Console.WriteLine("│3.Enter Grades          │");
-                    Console.WriteLine("│4.Show Grade by student │");
-                    Console.WriteLine("│5.Create folder         │");
-                    Console.WriteLine("│6.Exit                  │");
-                    Console.WriteLine("│════════════════════════│");
-                    Console.WriteLine("│Pick a number 1 to 6    │");
-                    Console.WriteLine("│Choose:                 │");
-                    Console.WriteLine("+========================+");
-
-                    Console.SetCursorPosition(8, 10);
-                    Menu = Console.ReadLine();
-
-                    Console.Clear();
-
-
-                    switch (Menu)
-                    {
-                        case "1":
-                            if (!Directory.Exists(mainFolder))
-                            {
-                                Console.WriteLine("Please create the folder first (Option 5).");
-                                System.Threading.Thread.Sleep(800);
-                                Console.SetCursorPosition(0, 0);
-                                Console.Clear();
-                            }
-
-                            else
-                            {
-                                RegisterStudent(studentSubjectFile);
-                            }
-                            break;
-                        case "2":
-                            if (!Directory.Exists(mainFolder))
-                            {
-                                Console.WriteLine("Please create the folder first (Option 5).");
-                                System.Threading.Thread.Sleep(800);
-                                Console.SetCursorPosition(0, 0);
-                                Console.Clear();
-
-                            }
-                            else
-                            {
-                                EnrollSubject(studentSubjectFile);
-                            }
-                            break;
-
-                        case "3":
-                            if (!Directory.Exists(mainFolder))
-                            {
-                                Console.WriteLine("Please create the folder first (Option 5).");
-                                System.Threading.Thread.Sleep(800);
-                                Console.SetCursorPosition(0, 0);
-                                Console.Clear();
-
-
-                            }
-                            else
-                            {
-                                EnterGrades(gradeFile);
-                            }
-                            break;
-
-
-                        case "4":
-                            ShowGrades(gradeFile);
-                            break;
-
-                        case "5":
-                            CreateFolder(mainFolder);
-                            break;
-
-                        case "6":
-                            running = false;
-                            return;
-                            break;
-
-                        default:
-                            Console.WriteLine("Invalid choice!");
+                    case "1":
+                        if (!Directory.Exists(mainFolder))
+                        {
+                            Console.WriteLine("Please create the folder first (Option 5).");
                             System.Threading.Thread.Sleep(800);
                             Console.SetCursorPosition(0, 0);
                             Console.Clear();
-                            break;
-                    }
+                        }
 
-                    Console.WriteLine("\nPress any key to continue...");
-                    Console.SetCursorPosition(0, 0);
-                    Console.ReadKey();
+                        else
+                        {
+                            RegisterStudent(studentSubjectFile);
+                        }
+                        break;
+                    case "2":
+                        if (!Directory.Exists(mainFolder))
+                        {
+                            Console.WriteLine("Please create the folder first (Option 5).");
+                            System.Threading.Thread.Sleep(800);
+                            Console.SetCursorPosition(0, 0);
+                            Console.Clear();
+
+                        }
+                        else
+                        {
+                            EnrollSubject(studentSubjectFile);
+                        }
+                        break;
+
+                    case "3":
+                        if (!Directory.Exists(mainFolder))
+                        {
+                            Console.WriteLine("Please create the folder first (Option 5).");
+                            System.Threading.Thread.Sleep(800);
+                            Console.SetCursorPosition(0, 0);
+                            Console.Clear();
+
+
+                        }
+                        else
+                        {
+                            EnterGrades(gradeFile);
+                        }
+                        break;
+
+
+                    case "4":
+                        ShowGrades(gradeFile);
+                        break;
+
+                    case "5":
+                        CreateFolder(mainFolder);
+                        break;
+
+                    case "6":
+                        running = false;
+                       
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid choice!");
+                        System.Threading.Thread.Sleep(800);
+                        Console.SetCursorPosition(0, 0);
+                        Console.Clear();
+                        break;
                 }
+
+                Console.WriteLine("\nPress any key to continue...");
+                Console.SetCursorPosition(0, 0);
+                Console.ReadKey();
             }
+        }
     }
 
     // =========================
@@ -186,23 +188,15 @@ class Program
         students.Add(s);
 
 
-        using (StreamWriter writer = new StreamWriter(file, true)) // And this file will be use to connect in RegisterStudent
+        string json = JsonSerializer.Serialize(students, new JsonSerializerOptions { WriteIndented = true });// And this file will be use to connect in RegisterStudent
         {
-            foreach (var connect in students)
-            {
-                writer.WriteLine("===== STUDENT =====");
-                writer.WriteLine($"\rName:{(connect.first_name) + ""} {(connect.middle_name) + ""}  {(connect.last_name)}");
-                writer.WriteLine("Birthdate: " + connect.year_birth);
-                writer.WriteLine("Age: " + connect.age_old);
-                writer.WriteLine("Address: " + connect.loaction_adress);
-                writer.WriteLine("Contact: " + connect.contact_number);
-                writer.WriteLine("Course/Year: " + connect.COURSE);
-                writer.WriteLine("------------------------");
-                System.Threading.Thread.Sleep(800);
+
+                File.WriteAllText(file, json);
+                 System.Threading.Thread.Sleep(800);
                 Console.SetCursorPosition(0, 0);
                 Console.Clear();
 
-            }
+            
 
 
         }
@@ -268,12 +262,14 @@ class Program
         if (!double.TryParse(Console.ReadLine(), out double first))
         {
             Console.WriteLine("Error please try again");
+            return;
         }
 
         Console.Write("2nd Semester Grade: ");
         if (!double.TryParse(Console.ReadLine(), out double second))
         {
             Console.WriteLine("Error please try again");
+            return;
         }
         double average = (first + second) / 2;
 
@@ -288,17 +284,15 @@ class Program
         grades.Clear();
         grades.Add(g);
 
-        using (StreamWriter writer = new StreamWriter(grad, true)) // The file is use to connect to enter grades 
-            foreach (var score in grades)
+        string json = JsonSerializer.Serialize(grades, new JsonSerializerOptions { WriteIndented = true });// The file is use to connect to enter grades 
+        
             {
-                writer.WriteLine("===== GRADES =====");
-                writer.WriteLine("Student: " + score.student_1);
-                writer.WriteLine("Student_ID: " + score.stude_ID);
-                writer.WriteLine("1st Sem: " + score.first_1);
-                writer.WriteLine("2nd Sem: " + score.second_2);
-                writer.WriteLine("Average: " + score.average_1);
-                writer.WriteLine("------------------------");
-            }
+
+            File.WriteAllText(grad, json);
+            System.Threading.Thread.Sleep(800);
+            Console.SetCursorPosition(0, 0);
+            Console.Clear();
+        }
 
         Console.WriteLine("Grades Saved!");
     }
@@ -314,7 +308,9 @@ class Program
 
         Console.Write("Enter Student Name: ");
         string search = Console.ReadLine();
-
+        bool found = false;
+        
+        
         if (File.Exists(grad))
         {
             using (StreamReader reader = new StreamReader(grad)) // is use to connect EnterGrades
@@ -331,12 +327,27 @@ class Program
                         Console.WriteLine(reader.ReadLine());
                         Console.WriteLine(reader.ReadLine());// when you keep pressing showgrade it will stop the input of the line to go down  
                     }
+                    
+
+
+
+                    
+
+
                 }
+                if (!found)
+                {
+                    Console.WriteLine("Student not found.");
+                }
+
             }
+
         }
-        else
+       else 
         {
-            Console.WriteLine("No grade file found.");
+            
+            
+            Console.WriteLine("No grade file found."); // this is use to indicate that the grade no yet made
         }
     }
 
@@ -344,7 +355,7 @@ class Program
     // 5 CREATE FOLDER
     // =========================
 
-    static void CreateFolder(string mainFolder)
+    static void CreateFolder(string mainFolder) // The string is use to conect to path combine and so it would be name as the file and will be going to the desktop and become a folder
     {
         Console.Clear();
 
@@ -359,3 +370,4 @@ class Program
         }
     }
 }
+
